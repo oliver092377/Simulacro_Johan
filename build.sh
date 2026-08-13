2c4cd263fdcf13cd69342107ecc1414b20538da0
Oliver <omayta@unsa.edu.pe>
2026-08-13 11:14:50 -0500

    cargar

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ python manage.py migrate --no-input
 python manage.py create_admin
 
 if [ "$CARGAR_DATOS" = "true" ]; then
-    python manage.py cargar_datos_prueba.py
+    python manage.py cargar_datos_prueba
 fi
 #python cargar_datos_prueba.py
 #python fix_alternativas.py
