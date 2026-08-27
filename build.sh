28b1325c39453432b3a3c01fedce2e66eba14531
Oliver <omayta@unsa.edu.pe>
2026-08-27 12:41:59 -0500

    edit build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-PLAYWRIGHT_BROWSERS_PATH=0 python -m playwright install chromium #recien incluido para que funcione en el contenedor de docker
+python -m playwright install chromium #recien incluido para que funcione en el contenedor de docker
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 
